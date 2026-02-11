8a4d12b40773e0e163dbeb102eeea18bfe2c6b21
Indio <indio.medeiros.dev@gmail.com>
2026-02-11 15:34:11 -0300

    Adiciona configuração inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import HomePage from "./pages/home/HomePage";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HomePage />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
