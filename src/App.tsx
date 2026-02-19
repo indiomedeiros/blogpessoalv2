2934c8f0f2e0fcfdde27344934c7eba810b7fc7e
Indio <indio.medeiros.dev@gmail.com>
2026-02-19 08:55:07 -0300

    Configura o React-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-import HomePage from "./pages/home/HomePage";
+import Home from "./pages/home/Home";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Header />
-      <HomePage />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <div className="grid grid-cols-1 grid-rows-[auto_1fr_auto] font-[Helvetica Neue] h-screen ">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
